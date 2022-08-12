b131f298ac0cdf4c1ff8cf9fe616a21a258560eb
Becher <mbelkhenchir@gmail.com>
2022-08-12 22:08:19 +0200

    Set input action to character and fix input bug

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +11,40 @@ public class Character : MonoBehaviour
         m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void OnEnable()
+    {
+        InputController.OnDash += Dash;
+        InputController.OnJump += Jump;
+        InputController.OnInteract += Interact;
+    }
+
+    private void OnDisable()
+    {
+        InputController.OnDash -= Dash;
+        InputController.OnJump -= Jump;
+        InputController.OnInteract -= Interact;
+    }
+
     void Update()
     {
-        
+        //Debug.Log(GetMoveInput());
+    }
+
+
+    private float GetMoveInput()
+    {
+        return InputController.GetInstance().moveDirection;
+    }
+    private void Jump()
+    {
+        Debug.Log("Jump");
+    }
+    private void Interact()
+    {
+        Debug.Log("Interact");
+    }
+    private void Dash()
+    {
+        Debug.Log("Dash");
     }
 }
---
+++ Assets/Scripts/Character/Controller/InputController.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    private static InputController instance;
+
+    public static InputController GetInstance()
+    {
+        return instance;
+    }
+    
+    private Inputs m_playerInput;
+    
+    public delegate void Interact();
+    public static event Interact OnInteract;
+    
+    public delegate void Jump();
+    public static event Jump OnJump;
+    
+    public delegate void Dash();
+    public static event Dash OnDash;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public float moveDirection { get; set; }
+
+    public void ReadMoveDirection(InputAction.CallbackContext _context)
+    {
+        moveDirection = _context.ReadValue<float>();
+    }
+    
+    public void ReadInteractAction(InputAction.CallbackContext _context)
+    {
+        if (_context.performed)
+            OnInteract?.Invoke();
+    }
+    
+    public void ReadDashAction(InputAction.CallbackContext _context)
+    {
+        if (_context.performed)
+            OnDash?.Invoke();
+    }
+    
+    public void ReadJumpAction(InputAction.CallbackContext _context)
+    {
+        if (_context.performed)
+            OnJump?.Invoke();
+    }
+    
+}

764e74f99e5eeefab99029b2677c3cf6caf8f1af
OpheliePELLOUXPRAYER <opheliepellouxprayer@gmail.com>
2022-08-13 00:57:06 +0200

    Add MultiShotsGunBehavior

--- Assets/Scripts/Robot/GunBehavior.cs
+++ Assets/Scripts/Robot/GunBehavior.cs
@@ -6,24 +6,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "GunBehavior/GunBehavior", order = 1)]
 public class GunBehavior : ScriptableObject
 {
-    [Serializable]
-    public struct Config
-    {
-        public float rate;
-        public float angleDegrees;
-    }
-
-    [SerializeField] private Config m_config;
-    [SerializeField] private GameObject m_bullet;
+    [SerializeField] protected float m_firingRate = 1.0f;
+    [SerializeField] protected float m_angleDegrees = 0.0f;
+    [SerializeField] protected GameObject m_bullet;
     
-    private float m_timer = 0.0f;
+    protected float m_timer = 0.0f;
 
-    public void Shoot(Vector3 startPos)
+    public virtual void Shoot(Vector3 startPos)
     {
         m_timer += Time.deltaTime;
-        if(m_timer >= m_config.rate)
+        if(m_timer >= m_firingRate)
         {
-            Quaternion rotation = Quaternion.Euler(0, 0, m_config.angleDegrees);
+            Quaternion rotation = Quaternion.Euler(0, 0, m_angleDegrees);
             Instantiate(m_bullet, startPos, rotation);
             m_timer = 0.0f;
         }

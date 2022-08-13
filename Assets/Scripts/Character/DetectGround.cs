b45f56481299b10c150b9f82d4cc45ac97c7ef99
Becher <mbelkhenchir@gmail.com>
2022-08-13 12:19:18 +0200

    Add detect robot and Add debug for character nav

--- Assets/Scripts/Character/DetectGround.cs
+++ Assets/Scripts/Character/DetectGround.cs
@@ -6,12 +6,11 @@ using UnityEngine;
 public class DetectGround : MonoBehaviour
 {
     private int m_isOnGround = 0;
-    private Animator m_fsm;
+    [SerializeField] private Animator m_fsm;
 
 
     private void Start()
     {
-        m_fsm = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,11 +19,13 @@ public class DetectGround : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.isTrigger || other.gameObject.layer == LayerMask.NameToLayer("Character")) return;
         ++m_isOnGround;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (other.isTrigger || other.gameObject.layer == LayerMask.NameToLayer("Character")) return;
         --m_isOnGround;
     }
 }

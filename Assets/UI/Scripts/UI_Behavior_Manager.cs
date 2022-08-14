37a72bfd1c7bc7f2c9dfc820323b151413606d5e
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2022-08-14 16:36:40 +0200

    UI First Commit (Behaviors Structure)

---
+++ Assets/UI/Scripts/UI_Behavior_Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Behavior_Manager : MonoBehaviour
+{
+    public string Behavior_Name;
+    public Sprite Behavior_Icon;
+    public Sprite Behavior_Icon_Selected;
+    public string Effect_Text;
+    public string Condition_Text;
+    public GunBehavior Behavior_Object;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

d15c373a7a4d5b3d0e104ea101474415a98cb843
OpheliePELLOUXPRAYER <opheliepellouxprayer@gmail.com>
2022-08-15 01:21:01 +0200

    add level manage and save manager

---
+++ Assets/Scripts/SaveDiskette.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
+
+public class SaveDiskette : MonoBehaviour
+{
+    [SerializeField] private List<GunBehavior> m_allUnlockedGunBehaviors;
+    [SerializeField] private List<GunBehavior> m_equippedGunBehaviors;
+
+    private Robot m_robot;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += InitRobotEquippedGunBehavior;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= InitRobotEquippedGunBehavior;
+    }
+
+    public void InitRobotEquippedGunBehavior(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Robot Init with equipped gun behavior");
+        m_robot = FindObjectOfType<Robot>();
+        if(m_robot)
+        {
+            m_robot.SetGunBehaviors(m_equippedGunBehaviors);
+        }
+    }
+
+    public void AddEquippedGunBehaviors(GunBehavior _equippedGunBehavior)
+    {
+        m_equippedGunBehaviors.Add(_equippedGunBehavior);
+    }
+
+    public void RemoveEquippedGunBehaviors(GunBehavior _equippedGunBehavior)
+    {
+        m_equippedGunBehaviors.Remove(_equippedGunBehavior);
+    }
+}

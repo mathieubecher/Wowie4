6c0ea9318a1891be643faffb23271ba1476d75f2
Becher <mbelkhenchir@gmail.com>
2022-08-14 17:13:04 +0200

    AngularSpeed for laser

--- Assets/Scripts/Enemy/Laser.cs
+++ Assets/Scripts/Enemy/Laser.cs
@@ -7,37 +7,50 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] private float m_dist = 10;
     [SerializeField] private float m_damagePerSeconds = 0.1f;
+    [SerializeField] private float m_angularSpeed = 30f;
     [SerializeField] private Transform m_shootSpawner;
 
-    private bool m_hasTarget = true;
     [SerializeField] private LifeManager m_target;
-    private LineRenderer m_laser;
 
+    private LineRenderer m_laser;
+    private bool m_hasTarget = true;
+    private Vector2 m_currentDir;
+    
     private void Awake()
     {
         m_target = FindObjectOfType<Character>().GetComponent<LifeManager>();
         m_laser = GetComponent<LineRenderer>();
+        m_currentDir = (m_target.transform.position - transform.position).normalized;
     }
 
     private void FixedUpdate()
     {
         if (m_hasTarget && (m_target == null || m_target.dead)) m_hasTarget = false;
-        Vector2 direction = m_target.transform.position - transform.position;
-        float distance = direction.magnitude;
+        
+        Vector2 desiredDirection =m_target.transform.position - transform.position;
+
+        float angle = Vector2.Angle(m_currentDir, desiredDirection);
+        m_currentDir = Vector3.Slerp(m_currentDir, desiredDirection, m_angularSpeed * Time.deltaTime / angle);
+        Vector2 direction = m_currentDir;
+        
+        float distance = desiredDirection.magnitude;
         
         if (m_hasTarget && distance < m_dist)
         {
             
-            LayerMask mask = LayerMask.GetMask("Default");
-            RaycastHit2D hit = Physics2D.Raycast(m_shootSpawner.transform.position, direction, distance, mask);
+            int mask = 1 << LayerMask.NameToLayer("Default") | 1 << m_target.gameObject.layer;
+            RaycastHit2D hit = Physics2D.Raycast(m_shootSpawner.transform.position, direction, m_dist, mask);
             if (hit)
             {
                 DrawLineRenderer(hit.point);
+                if (hit.collider.gameObject.layer == m_target.gameObject.layer)
+                {
+                    m_target.Damaged(m_damagePerSeconds * Time.deltaTime);
+                }
             }
             else
             {
-                m_target.Damaged(m_damagePerSeconds * Time.deltaTime);
-                DrawLineRenderer(m_target.transform.position + Vector3.up);
+                DrawLineRenderer((Vector2)transform.position + direction * m_dist);
             }
             
         }
